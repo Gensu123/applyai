66775453d2b356d6ab952cb43d00b3c0437a8f04
Nukelimer <112999689+Nukelimer@users.noreply.github.com>
2024-05-05 18:06:55 +0100

    testing.

--- src/app/(dashboard)/(routes)/dashboard/(DashBoard-Pages)/DashboardContent.tsx
+++ src/app/(dashboard)/(routes)/dashboard/(DashBoard-Pages)/DashboardContent.tsx
@@ -1,102 +1,107 @@
 // pages/dashboard.js or a similar file
-'use client'
+"use client";
 
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import { IoIosCloudUpload } from 'react-icons/io'; // Make sure you have installed the react-icons package
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { IoIosCloudUpload } from "react-icons/io"; // Make sure you have installed the react-icons package
 import { BsBriefcaseFill } from "react-icons/bs";
 import { IoSchoolSharp } from "react-icons/io5";
 import { BsBookFill } from "react-icons/bs";
 import { MdOutlinePalette } from "react-icons/md";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const DashboardContent = () => {
   return (
     <div className="p-8">
-    <div className="flex items-center justify-between   rounded-lg">
-      <div className="flex items-center space-x-4 flex-grow ">
-        {/* Profile image */}
-        <Image src="/Test.png" alt="Olivia Simmons" width={187} height={187} className="rounded-full shadow-black shadow-xl " />
-        {/* Greeting message */}
-        <div className="min-w-0 p-10 ">
-          <h2 className="text-5xl font-bold text-gray-700  pb-4">Hey, I am Front-end Developer,</h2>
-          <h3 className="text-4xl text-gray-500  pb-4">Based in Brazil 🇧🇷</h3>
-          <p className="text-gray-700 text-2xl  " >Olivia Simmons</p>
+      <div className="flex items-center justify-between   rounded-lg">
+        <div className="flex items-center space-x-4 flex-grow ">
+          {/* Profile image */}
+          <Image
+            src="/Test.png"
+            alt="Olivia Simmons"
+            width={187}
+            height={187}
+            className="rounded-full shadow-black shadow-xl "
+          />
+          {/* Greeting message */}
+          <div className="min-w-0 p-10 ">
+            <h2 className="text-5xl font-bold text-slate-700  pb-4">
+              Hey, I am Front-end Developer,
+            </h2>
+            <h3 className="text-4xl text-slate-500  pb-4">
+              Based in Brazil 🇧🇷
+            </h3>
+            <p className="text-slate-700 text-2xl  ">Olivia Simmons</p>
+          </div>
+        </div>
+        {/* Upload button */}
+        <div className="flex-shrink-0 ml-4 pr-[118px]  ">
+          <Button className=" w-[223px] h-[72px] flex items-center  bg-purple-500 hover:bg-purple-600 text-white text-md font-extrabold rounded-xl">
+            Upload Resume <IoIosCloudUpload size={20} className="ml-2" />
+          </Button>
         </div>
       </div>
-      {/* Upload button */}
-      <div className="flex-shrink-0 ml-4 pr-[118px]  ">
-        <Button className=" w-[223px] h-[72px] flex items-center  bg-purple-500 hover:bg-purple-600 text-white text-md font-extrabold rounded-xl">
-          Upload Resume <IoIosCloudUpload size={20} className="ml-2" />
-        </Button>
-      </div>
-    </div>
 
-    <div className="flex justify-center items-center  ">
-      
-  <div className="grid grid-cols-2 gap-8">
-    {/* Work Experience */}
-    <div className='border-2 border-grey-100 rounded-3xl'>
-    <div className="bg-purple-200 p-6 rounded-3xl shadow-xl w-[631px] h-[259px] border-white border-4">
-      <h3 className="text-3xl font-semibold mb-2">Work Experience:</h3>
-      
-      <div className="flex items-center  space-x-4 h-44 ">
-      <BsBriefcaseFill size={40} className="text-purple-600"/>
-        <div>
-          <p className="text-2xl font-semibold">Executive Manager</p>
-          <p className="text-xl">3 Years - Gorme Pharmaceuticals</p>
+      <div className="flex justify-center items-center  ">
+        <div className="grid grid-cols-2 gap-8">
+          {/* Work Experience */}
+          <div className="border-2 border-slate-200 rounded-3xl">
+            <div className="bg-purple-200 p-6 rounded-3xl shadow-xl w-[631px] h-[259px] border-white border-4">
+              <h3 className="text-3xl font-semibold mb-2">Work Experience:</h3>
+
+              <div className="flex items-center  space-x-4 h-44 ">
+                <BsBriefcaseFill size={40} className="text-purple-600" />
+                <div>
+                  <p className="text-2xl font-semibold">Executive Manager</p>
+                  <p className="text-xl">3 Years - Gorme Pharmaceuticals</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Education */}
+          <div className="border-2 border-slate-200 rounded-3xl">
+            <div className="bg-[#C1F4E6] p-6 rounded-3xl shadow-xl w-[631px] h-[259px] border-white border-4">
+              <h3 className="text-3xl font-semibold mb-2">Education:</h3>
+              <div className="flex items-center  space-x-4 h-44 ">
+                <IoSchoolSharp size={40} className="text-purple-600" />
+                <div>
+                  <p className="text-2xl font-semibold">Bachelor</p>
+                  <p className="text-xl">TUMS University of Engineering</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Skills */}
+          <div className="border-2 border-slate-200 rounded-3xl">
+            <div className="bg-[#FAE0F2] p-6 rounded-3xl shadow-xl w-[631px] h-[259px] border-white border-4">
+              <h3 className="text-3xl font-semibold mb-2">Skills:</h3>
+              <div className="flex items-center  space-x-4 h-44 ">
+                <MdOutlinePalette size={40} color="#FF7AD6 " />
+                <div>
+                  <p className="text-2xl font-semibold">UI Designer</p>
+                  <p className="text-xl">6 Month Internship</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Hobbies */}
+          <div className="border-2 border-slate-200 rounded-3xl">
+            <div className="bg-[#CBDAFA] p-6 rounded-3xl shadow-xl w-[631px] h-[259px] border-white border-4">
+              <h3 className="text-3xl font-semibold mb-2">Hobbies:</h3>
+
+              <div className="flex items-center  space-x-4 h-44 ">
+                <BsBookFill size={40} color="#447EFA" />
+
+                <div>
+                  <p className="text-2xl font-semibold">Reading Book</p>
+                  <p className="text-xl">700 Pages a Day</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    </div>
-    {/* Education */}
-    <div className='border-2 border-grey-100 rounded-3xl'>
-    <div className="bg-[#C1F4E6] p-6 rounded-3xl shadow-xl w-[631px] h-[259px] border-white border-4">
-      <h3 className="text-3xl font-semibold mb-2">Education:</h3>
-      <div className="flex items-center  space-x-4 h-44 ">
-        <IoSchoolSharp  size={40} className="text-purple-600"/>
-      <div>
-      <p className="text-2xl font-semibold">Bachelor</p>
-      <p className='text-xl'>TUMS University of Engineering</p>
-      </div>
-    </div>
-    </div>
-</div>
-    {/* Skills */}
-    <div className='border-2 border-grey-100 rounded-3xl'>
-
-    <div className="bg-[#FAE0F2] p-6 rounded-3xl shadow-xl w-[631px] h-[259px] border-white border-4">
-      <h3 className="text-3xl font-semibold mb-2">Skills:</h3>
-      <div className="flex items-center  space-x-4 h-44 ">
-      <MdOutlinePalette   size={40} color='#FF7AD6 ' />
-        <div>
-      <p className="text-2xl font-semibold">UI Designer</p>
-      <p className='text-xl'>6 Month Internship</p>
-      </div>
-    </div>
-    </div>
-    </div>
-    {/* Hobbies */}
-    <div className='border-2 border-grey-100 rounded-3xl'>
-
-    <div className="bg-[#CBDAFA] p-6 rounded-3xl shadow-xl w-[631px] h-[259px] border-white border-4">
-      <h3 className="text-3xl font-semibold mb-2">Hobbies:</h3>
-      
-
-      <div className="flex items-center  space-x-4 h-44 ">
-        <BsBookFill  size={40} color='#447EFA' />
-
-        <div>
-      <p className="text-2xl font-semibold">Reading Book</p>
-      <p className='text-xl'>700 Pages a Day</p>
-      </div>
-    </div>
-    </div>
-    </div>
-  </div>
-</div>
-
     </div>
   );
 };
--- src/app/(landingpage)/faq.tsx
+++ src/app/(landingpage)/faq.tsx
@@ -1,93 +1,126 @@
-import { useState, useEffect } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { MdCancel } from "react-icons/md";
+import { ArrowBigDownIcon, ChevronDown } from "lucide-react";
 
 const Faq = () => {
-    const [selectedId, setSelectedId] = useState<string | null>(null);
-    useEffect(() => {
-        AOS.init({
-            duration: 1200,
-            once: true, // Animations will only play once
-        });
-    }, []);
+  const [selectedId, setSelectedId] = useState<string | null>(null);
+  useEffect(() => {
+    AOS.init({
+      duration: 1200,
+      once: true, // Animations will only play once
+    });
+  }, []);
 
-    const faqs = [
-        { id: 'faq1', question: "How does ApplyAI match me with jobs?", answer: "We use advanced AI to match your skills and preferences with job listings." },
-        { id: 'faq2', question: "Is ApplyAI free to use?", answer: "Yes, there's a free Basic plan, with premium features available for a subscription." },
-        { id: 'faq3', question: "Can I customize my job preferences on ApplyAI?", answer: "Absolutely! You can set preferences for job type, location, salary, and more." },
-        { id: 'faq4', question: "How often are new job listings updated on ApplyAI?", answer: "Job listings are updated daily to ensure you see the most current opportunities." },
-        { id: 'faq5', question: "Is ApplyAI suitable for both professionals and entry-level job seekers?", answer: "Yes, ApplyAI is designed for job seekers at all career levels." },
-    ];
+  const faqs = [
+    {
+      id: "faq1",
+      question: "How does ApplyAI match me with jobs?",
+      answer:
+        "We use advanced AI to match your skills and preferences with job listings.",
+    },
+    {
+      id: "faq2",
+      question: "Is ApplyAI free to use?",
+      answer:
+        "Yes, there's a free Basic plan, with premium features available for a subscription.",
+    },
+    {
+      id: "faq3",
+      question: "Can I customize my job preferences on ApplyAI?",
+      answer:
+        "Absolutely! You can set preferences for job type, location, salary, and more.",
+    },
+    {
+      id: "faq4",
+      question: "How often are new job listings updated on ApplyAI?",
+      answer:
+        "Job listings are updated daily to ensure you see the most current opportunities.",
+    },
+    {
+      id: "faq5",
+      question:
+        "Is ApplyAI suitable for both professionals and entry-level job seekers?",
+      answer: "Yes, ApplyAI is designed for job seekers at all career levels.",
+    },
+  ];
 
-    return (
-        <div className="pt-32 p-4 sm:p-12 lg:p-24">
-            <h1 className="text-2xl sm:text-3xl font-bold text-center mb-6" data-aos="fade-up">FAQ</h1>
-            <div className="max-w-3xl mx-auto text-center">
-                {faqs.map((faq, index) => (
-                    <motion.div 
-                      key={faq.id} 
-                      layoutId={faq.id} 
-                      onClick={() => setSelectedId(selectedId === faq.id ? null : faq.id)} 
-                      className="mb-4 p-4 sm:p-6 bg-gray-100 rounded-md shadow-sm cursor-pointer"
-                      data-aos="fade-up"
-                      data-aos-delay={`${index * 100}`}
-                      aria-expanded={selectedId === faq.id}
-                      role="button"
+  return (
+    <div className="pt-32 p-4  md:p-24">
+      <h1
+        className="text-2xl sm:text-3xl font-bold text-center mb-6"
+        data-aos="fade-up">
+        FAQ
+      </h1>
+      <div className="max-w-3xl mx-auto text-center">
+        {faqs.map((faq, index) => (
+          <motion.div
+            key={faq.id}
+            layoutId={faq.id}
+            onClick={() => setSelectedId(selectedId === faq.id ? null : faq.id)}
+            className="mb-4 p-4 sm:p-6 bg-slate-200 rounded-md shadow-sm cursor-pointer"
+            data-aos="fade-up"
+            data-aos-delay={`${index * 100}`}
+            aria-expanded={selectedId === faq.id}
+            role="button">
+            <p className="text-md sm:text-lg font-medium">{faq.question}</p>
+            <span className="text- text-slate-900 text-xl flex justify-center  mt-2">
+              {selectedId === faq.id ? "-" : <ChevronDown/>}
+            </span>
+            {selectedId === faq.id && (
+              <motion.p className="text-sm sm:text-md mt-2  ">
+                {faq.answer}
+              </motion.p>
+            )}
+          </motion.div>
+        ))}
+      </div>
 
-                    >
-                        <p className="text-md sm:text-lg font-medium">{faq.question}</p>
-                        <span className="text-blue-600 text-xl">{selectedId === faq.id ? '-' : '+'}</span>
-                        {selectedId === faq.id && (
-                            <motion.p className="text-sm sm:text-md mt-2  ">
-                                {faq.answer}
-                            </motion.p>
-                        )}
-                    </motion.div>
+      <AnimatePresence>
+        {selectedId && (
+          <motion.div
+            className="fixed inset-0 z-50 flex justify-center items-center"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}>
+            <motion.div
+              className="absolute inset-0 bg-black bg-opacity-25"
+              onClick={() => setSelectedId(null)}
+              initial={{ backdropFilter: "blur(0px)" }}
+              animate={{ backdropFilter: "blur(4px)" }}
+              transition={{ duration: 0.25 }}
+            />
+            <motion.div
+              className="relative p-4 sm:p-6 bg-white rounded-md shadow-lg max-w-lg m-4 text-center"
+              layoutId={selectedId}
+              initial={{ y: 50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: 50, opacity: 0 }}
+              transition={{ duration: 0.25 }}>
+              {/* I  REMOVED THE X LETTER AND USED AN ICON FROM LUCIDE REACT FOR ASTHETIC PURPOSES. I REMOVE M ARGIN TOO BECASE FOR LONGER HEADINGS, IT WAS GROWING INTO THE BUTTON AND DISRUPTING UX. */}
+              <motion.button
+                className="absolute top-0 right-0  w-10 h-10"
+                onClick={() => setSelectedId(null)}>
+                <MdCancel size={30} />
+              </motion.button>
+              {faqs
+                .filter((faq) => faq.id === selectedId)
+                .map((filteredFaq) => (
+                  <motion.div key={filteredFaq.id}>
+                    <motion.h5 className="text-lg font-medium border-b p-2 border-slate-300">
+                      {filteredFaq.question}
+                    </motion.h5>
+                    <motion.p className="p-2">{filteredFaq.answer}</motion.p>
+                  </motion.div>
                 ))}
-            </div>
-
-            <AnimatePresence>
-                {selectedId && (
-                    <motion.div 
-                      className="fixed inset-0 z-50 flex justify-center items-center"
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: 1 }}
-                      exit={{ opacity: 0 }}
-                    >
-                        <motion.div 
-                          className="absolute inset-0 bg-black bg-opacity-25"
-                          onClick={() => setSelectedId(null)}
-                          initial={{ backdropFilter: 'blur(0px)' }}
-                          animate={{ backdropFilter: 'blur(4px)' }}
-                          transition={{ duration: 0.25 }}
-                        />
-                        <motion.div 
-                          className="relative p-4 sm:p-6 bg-white rounded-md shadow-lg max-w-lg m-4 text-center"
-                          layoutId={selectedId}
-                          initial={{ y: 50, opacity: 0 }}
-                          animate={{ y: 0, opacity: 1 }}
-                          exit={{ y: 50, opacity: 0 }}
-                          transition={{ duration: 0.25 }}
-                        >
-                            <motion.button 
-                              className="absolute top-0 right-0 m-2"
-                              onClick={() => setSelectedId(null)}
-                            >
-                              X
-                            </motion.button>
-                            {faqs.filter(faq => faq.id === selectedId).map((filteredFaq) => (
-                                <motion.div key={filteredFaq.id}>
-                                    <motion.h5 className="text-lg font-medium border-b p-2 border-gray-300">{filteredFaq.question}</motion.h5>
-                                    <motion.p className='p-2'>{filteredFaq.answer}</motion.p>
-                                </motion.div>
-                            ))}
-                        </motion.div>
-                    </motion.div>
-                )}
-            </AnimatePresence>
-        </div>
-    );
-}
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
 
 export default Faq;
